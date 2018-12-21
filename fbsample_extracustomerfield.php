061954916727e83e1f086d66bf44a615d2636ffd
Frédéric BENOIST <frederic.benoist@free.fr>
2018-12-21 16:07:00 +0100

    Fix httpS and remove email

--- fbsample_extracustomerfield.php
+++ fbsample_extracustomerfield.php
@@ -12,16 +12,9 @@
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
  *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- *
- *  @author    Frédéric BENOIST <http://www.fbenoist.com/>
- *  @copyright 2013-2018 Frédéric BENOIST <contact@fbenoist.com>
+ *  @author    Frédéric BENOIST
+ *  @copyright 2013-2018 Frédéric BENOIST <https://www.fbenoist.com/>
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- *  International Registered Trademark & Property of PrestaShop SA
  */
 
 class FbSample_ExtraCustomerField extends Module
